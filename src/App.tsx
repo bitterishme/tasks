e329ac0dd485a6017ffea59716ef2f5d0735bdb3
Meghann Manson <meghann.manson@maine.edu>
2025-02-26 13:41:52 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Meghann Manson
+                automatically reload. Meghann Manson Hello World
             </p>
         </div>
     );
