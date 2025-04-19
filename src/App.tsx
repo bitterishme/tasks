5bf946ea790a8c5b0ac5db94665c04011af60a97
meghann <meghann.manson@maine.edu>
2025-04-19 15:42:38 -0400

    first task solved

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): React.JSX.Element {
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Meghann Manson
+                Hello World. Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload. Meghann Manson
             </p>
         </div>
     );
