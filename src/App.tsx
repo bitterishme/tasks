b8770a1c1658d56a00b6929b1184f702247f2fcc
meghann <meghann.manson@maine.edu>
2025-04-20 09:20:16 -0400

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header" style={{ background: "red" }}>
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1> Heading </h1>
+            <img src="image.jpg" alt="Image" />
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Meghann Manson
